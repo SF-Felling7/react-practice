9791819e71d54c9c4a5590bad182011320efc5c7
Sean Felling <Snfelling7@gmail.com>
2017-08-09 20:22:42 -0500

    removed scripts

--- public/scripts/script.jsx
+++
@@ -1,9 +0,0 @@
-var Hello = React.createClass({
-  render: function() {
-    return (
-      <h2> What's up React? I'm Sean</h2>
-      );
-  }
-});
-
-React.render(<Hello/>,document.getElementById("root"));
